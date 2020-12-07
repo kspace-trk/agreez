a4df07edf29eccc5e661aefea05136ea1fa34434
kspace-trk <keigonakao.0825@gmail.com>
2020-12-07 19:10:26 +0900

    [fix]gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '30689c0716c592c85ee5c41ea1caf4bf899424ef',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30689c0716c592c85ee5c41ea1caf4bf899424ef',
+    ),
+  ),
+);
